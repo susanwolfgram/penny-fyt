55f0be77b7acd4f376ab4b61b1a25b8623f8480f
Susan-Wolfgram <skwolf@uw.edu>
2017-04-20 17:21:29 -0700

    signup and login changes

--- js/penny.js
+++ js/penny.js
@@ -2,23 +2,29 @@ var app = angular.module("myApp", ["firebase"]);
 
 app.controller("myCtrl", function($scope, $firebaseObject, $firebaseArray, $firebaseAuth, $interval) {
 	var user;
-
 	$scope.signup = function () {
 		if ($scope.pwd == $scope.pwd2) {
 			var userRef = firebase.database().ref().child("users");
 			// create a synchronized array
 			$scope.users = $firebaseArray(userRef);
 
-			console.log($scope.users);
-			$scope.users.$add({
-				fName: $scope.fName, 
-				lName: $scope.lName
-			});
+
 
 			firebase.auth().createUserWithEmailAndPassword($scope.email, $scope.pwd).then(function(userData) {
 			  	console.log("User " + userData.uid + " created successfully!");
 			  	user = firebase.auth().currentUser;
 				console.log(user);
+				console.log($scope.users);
+				var id = userData.uid;
+				// $scope.users.$add({
+				// 	fName: $scope.fName, 
+				// 	lName: $scope.lName
+				// });
+				firebase.database().ref().child("users").child(userData.uid).set({
+			      name: $scope.fName
+			    });
+				// var form = document.getElementById("signupForm");
+				// form.reset();
 			}).catch(function(error) {
 			  console.error("Error: ", error);
 			});
@@ -40,6 +46,37 @@ app.controller("myCtrl", function($scope, $firebaseObject, $firebaseArray, $fire
 			document.getElementById("pwd2").style.border = "";
 			document.getElementById("pwdMessage").innerHTML = "";
 		}
+	}
+
+	$scope.login = function() {
+		console.log("login");
+		firebase.auth().signInWithEmailAndPassword($scope.loginEmail, $scope.loginPwd).then(function(userData) {
+			user = firebase.auth().currentUser;
+			console.log(user);
+			user = userData.uid; 
+	     	currentUser = firebase.database().ref().child("users").child(user);
+			userObj = $firebaseObject(currentUser);
+			console.log(userObj);
+			var form = document.getElementById("loginForm");
+			form.reset();
+			window.location = "discover.html";
+			$scope.loggedIn = true; 
+			$scope.user = user; 
+		}).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  // ...
+		});
+	}
+
+	$scope.logOut = function() {
+		console.log("log out");
+		firebase.auth().signOut().then(function() {
+		  $scope.loggedIn = false; 
+		}).catch(function(error) {
+		  // An error happened.
+		});
 	}
 
 	
